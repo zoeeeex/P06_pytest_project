e2e9d6a0d87af2ec1a8c8ae78820b96e16959cc2
zoe@MSI <23014711@myrp.edu.sg>
2024-05-21 15:47:50 +0800

    initial commit

---
+++ tests/test_calculator.py
@@ -0,0 +1,15 @@
+from calculator.calculator import Calculator
+
+class TestCalculator:
+    def test_add(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
